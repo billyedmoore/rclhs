58fdb83c7e56e4002431e6be4e394be58df8ad02
Billy Moore <billyedmoore@yahoo.com>
2026-02-25 12:30:10 +0000

    Create and manage the context for a ROS2 session.

--- c-wrap/wrap.c
+++ c-wrap/wrap.c
@@ -1,26 +1,90 @@
 #include "wrap.h"
+#include "rcl/context.h"
+#include "rcl/init.h"
+#include "rcl/init_options.h"
 #include <stdio.h>
 #include <rcutils/error_handling.h>
 
+/*
+ * Init ROS and return the new Context.
+ *
+ * Lifecycle of a context can be seen:
+ *  https://docs.ros.org/en/rolling/p/rcl/generated/structrcl__context__s.html
+*/
+Context* create_context(){
+    rcl_ret_t return_code = RCL_RET_OK;
+
+    rcl_context_t rcl_context = rcl_get_zero_initialized_context();
+    Context *context  = malloc(sizeof(Context));
+    context->context = rcl_context;
+
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+    rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+    return_code = rcl_init_options_init(&init_options, allocator);
+
+    if (return_code != RCL_RET_OK){
+        free(context);
+        return NULL; 
+    }
+
+    // Argc and Argv not passed for now
+    return_code = rcl_init(0, NULL, &init_options, &context->context);
+
+    if (return_code != RCL_RET_OK){
+        return_code = rcl_init_options_fini(&init_options);
+        if (return_code != RCL_RET_OK){
+            // TODO: log
+        }
+        free(context);
+        return NULL;
+    }
+
+    return_code = rcl_init_options_fini(&init_options);
+
+    if (return_code != RCL_RET_OK){
+        shutdown_context(context);
+        return NULL; 
+    }
+
+    return context;
+}
+
+/*
+ * Shutdown ROS and destroy the Context.
+ */
+void shutdown_context(Context* context){
+    rcl_ret_t return_code = RCL_RET_OK;
+
+    return_code = rcl_shutdown(&context->context);
+    if (return_code != RCL_RET_OK){
+        // TODO: log
+    }
+    return_code = rcl_context_fini(&context->context);
+    if (return_code != RCL_RET_OK){
+        // TODO: log
+    }
+
+    free(context);
+}
+
 Node* create_node(const char *node_name,
                   const char *namespace,
-                  rcl_context_t *context){
+                  Context *context){
     rcl_ret_t return_code = RCL_RET_OK;
     rcl_node_t rcl_node = rcl_get_zero_initialized_node();
     rcl_node_options_t node_opt = rcl_node_get_default_options();
-    return_code = rcl_node_init(&rcl_node,node_name,namespace,context,&node_opt);
+    return_code = rcl_node_init(&rcl_node,node_name,namespace,&context->context,&node_opt);
 
     Node *node = malloc(sizeof(Node));
-    node->node = &rcl_node;
+    node->node = rcl_node;
 
     if (return_code != RCL_RET_OK){
         fprintf(stderr,"[C] Failed to init node (name - \"%s\", namespace - \"%s\") ERR - %s.\n",
                 node_name,namespace,rcutils_get_error_string().str);
         destroy_node(node);
-        return 0;
+        return NULL;
     }
     
-    // TODO: remove once working
     printf("[C] Successfully created node.\n");
 
     return node;
@@ -28,14 +92,15 @@ Node* create_node(const char *node_name,
 
 void destroy_node(Node* node){
     rcl_ret_t return_code = RCL_RET_OK;
-    return_code = rcl_node_fini(node->node);
+    return_code = rcl_node_fini(&node->node);
+    free(node);
 
     if (return_code != RCL_RET_OK){
         fprintf(stderr,"[C] Failed to destory node, ERR - %s.\n",rcutils_get_error_string().str);
     }
     else {
-        // TODO: remove once working
         printf("[C] Successfully destoryed node.\n");
     }
+
 }
 
--- src/RclHs/Bindings.hs
+++ src/RclHs/Bindings.hs
@@ -1,7 +1,8 @@
-module RclHs.Bindings (createNode, Node, Context) where
+module RclHs.Bindings (createNode, createContext, Node, Context) where
 
-import Foreign (ForeignPtr, FunPtr, Ptr, newForeignPtr, withForeignPtr)
+import Foreign (ForeignPtr, FunPtr, Ptr, newForeignPtr, touchForeignPtr, withForeignPtr)
 import Foreign.C (CString, withCString)
+import Foreign.Concurrent qualified as FC
 
 -- Opaque types
 data Node
@@ -10,7 +11,11 @@ data Context
 
 foreign import capi "wrap.h create_node" c_createNodeRawPtr :: CString -> CString -> Ptr Context -> IO (Ptr Node)
 
-foreign import capi "wrap.h &destroy_node" c_destoryNodeRawPtr :: FunPtr (Ptr Node -> IO ())
+foreign import capi "wrap.h destroy_node" c_destoryNodeRawPtr :: Ptr Node -> IO ()
+
+foreign import capi "wrap.h create_context" c_createContextRawPtr :: IO (Ptr Context)
+
+foreign import capi "wrap.h &shutdown_context" c_shutdownContextRawPtr :: FunPtr (Ptr Context -> IO ())
 
 createNode :: String -> String -> ForeignPtr Context -> IO (ForeignPtr Node)
 createNode name namespace context =
@@ -18,4 +23,13 @@ createNode name namespace context =
     withCString namespace $ \c_namespace ->
       withForeignPtr context $ \c_context -> do
         ptr <- c_createNodeRawPtr c_name c_namespace c_context
-        newForeignPtr c_destoryNodeRawPtr ptr
+        FC.newForeignPtr ptr $ do
+          c_destoryNodeRawPtr ptr
+          touchForeignPtr context
+
+-- It will likely be cleaner to move to a withContext pattern
+-- at sometime.
+createContext :: IO (ForeignPtr Context)
+createContext = do
+  ptr <- c_createContextRawPtr
+  newForeignPtr c_shutdownContextRawPtr ptr
